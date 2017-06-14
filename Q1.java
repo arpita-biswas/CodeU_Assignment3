0af1ab77c42018c02d78dd4aaea12a6d143d2e81
Arpita Biswas <arpita.biswas@live.in>
2017-06-15 02:45:39 +0530

    Incorporated reviews in Q1.java

--- Q1.java
+++ Q1.java
@@ -1,4 +1,3 @@
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -16,8 +15,7 @@ import java.util.Set;
 import org.junit.Test;
 
 public class Q1 {
-	public enum Moves { N, NE, E, SE, S, SW, W, NW };
-	
+
 	/**
 	 * getAllWords() finds all the "dictionary" words from a given "grid"
 	 * (having "n_row" rows and "n_column" number of columns), 
@@ -55,7 +53,7 @@ public class Q1 {
 				// for each cell, call the getAllWordsHelper() function to create all 
 				//dictionary words creating by valid traversal starting from that cell
 				ArrayList<GridPoint> visited = new ArrayList<GridPoint>();
-				Set<String> temp = getAllWordsHelper(grid, dictionary, i, j, visited);
+				Set<String> temp = getAllWordsHelper(grid, dictionary, i, j, visited, new StringBuilder());
 				//getAllWordsHelper() return null when no valid word is return starting from that cell
 				//if it does not return null, we add the valid words in the set of allWords
 				if(temp!=null){
@@ -73,12 +71,8 @@ public class Q1 {
 	}
 	
 	/**
-	 * move() takes as input a cell, represented by (r,c) in the grid, and checks whether 
-	 * the "direction" specified is a valid direction to find a longer word.
-	 * It checks whether or not the direction specified leads to a location not in the grid.
-	 * Finally, if the direction is valid, this function returns 
-	 * the new cell obtained by taking this "direction" from (r,c).
-	 * If invalid, it returns null.
+	 * move() takes as input a cell, represented by (r,c) in the grid, and returns
+	 * a set of valid next grid points to continue search.
 	 * 
 	 * @param r
 	 * @param c
@@ -86,57 +80,33 @@ public class Q1 {
 	 * @param n_column
 	 * @return
 	 */
-	private static GridPoint move(Q1.Moves direction, int r, int c, int n_row, int n_column){
-		
-		if(direction == Q1.Moves.N){
-			if(r == 0 ){
-				return null;
-			}
-			return new GridPoint(r-1, c);
+	private static Set<GridPoint> move(int r, int c, int n_row, int n_column){
+		Set<GridPoint> valid_directions = new HashSet<GridPoint>();
+		if(r != 0 ){
+				valid_directions.add(new GridPoint(r-1, c));
 		}
-		if(direction == Q1.Moves.NE){
-			if(r == 0 || c == n_column-1){
-				return null;
-			}
-			return new GridPoint(r-1, c+1);
+		if(!(r == 0 || c == n_column-1)){
+				valid_directions.add(new GridPoint(r-1, c+1));
 		}
-		if(direction == Q1.Moves.E){
-			if(c == n_column-1){
-				return null;
-			}
-			return new GridPoint(r, c+1);
+		if(c != n_column-1){
+				valid_directions.add(new GridPoint(r, c+1));
 		}
-		if(direction == Q1.Moves.SE){
-			if(r == n_row-1 || c == n_column-1){
-				return null;
-			}
-			return new GridPoint(r+1, c+1);
+		if(!(r == n_row-1 || c == n_column-1)){
+			valid_directions.add(new GridPoint(r+1, c+1));
 		}
-		if(direction == Q1.Moves.S){
-			if(r == n_row-1){
-				return null;
-			}
-			return new GridPoint(r+1, c);
+		if(r != n_row-1){
+				valid_directions.add(new GridPoint(r+1, c));
 		}
-		if(direction == Q1.Moves.SW){
-			if(r == n_row-1 || c == 0){
-				return null;
-			}
-			return new GridPoint(r+1, c-1);
+		if(!(r == n_row-1 || c == 0)){
+			valid_directions.add(new GridPoint(r+1, c-1));
 		}
-		if(direction == Q1.Moves.W){
-			if(c == 0){
-				return null;
-			}
-			return new GridPoint(r, c-1);
+		if(c != 0){
+				valid_directions.add(new GridPoint(r, c-1));
 		}
-		if(direction == Q1.Moves.NW){
-			if(r == 0 || c == 0){
-				return null;
-			}
-			return new GridPoint(r-1, c-1);
+			if(!(r == 0 || c == 0)){
+				valid_directions.add(new GridPoint(r-1, c-1));
 		}
-		return null;
+		return valid_directions;		
 	}
 	
 	/** getAllWordsHelper() finds all words that can be formed with the prefix specified
@@ -147,51 +117,42 @@ public class Q1 {
 	 * @param r : row number of the cell to be processed
 	 * @param c : column number of the cell to be processed
 	 * @param visited : A list of cells that forms the prefix
+	 * @param word_temp : The "potential" prefix
 	 * @return
 	 */
-	private static Set<String> getAllWordsHelper(char [][] grid, Dictionary dictionary, int r, int c, ArrayList<GridPoint> visited){
+	private static Set<String> getAllWordsHelper(char [][] grid, Dictionary dictionary, int r, int c, ArrayList<GridPoint> visited, StringBuilder word_temp){
 		Set<String> allStr = new HashSet<String>();
-		//System.out.println("At ("+r+","+c+")");
-		String word = new String();
-		
-		//Create the "potential" prefix string from "visited" list
-		for(int i=0; i<visited.size(); i++){
-			word = word.concat(Character.toString(grid[visited.get(i).r][visited.get(i).c]));
-		}
-		//System.out.print(word+"->");
+	
 		//append this character (r,c) to the "potential" prefix
-		word = word + Character.toString(grid[r][c]);
-		//System.out.println(word);
+		word_temp = word_temp.append(Character.toString(grid[r][c]));
+
+		String word = word_temp.toString();
+
+		//append this cell (r,c) to the visited list
+		visited.add(new GridPoint(r,c));
 		
 		//If the "potential" prefix string is not a valid prefix in the dictionary,
 		//then return an empty set of strings
 		if(!dictionary.isPrefix(word)){
-			//System.out.println("not a prefix");
 			return allStr;
 		}
-		
-		//If the "potential" prefix is indeed a prefix of some valid word,
-		//then, append this cell (r,c) to the visited list
-		//System.out.println("is a prefix");
-		visited.add(new GridPoint(r,c));
 		
 		//If "potential" prefix is a valid word,
 		//then add it to the list of allStr to be returned
 		if(dictionary.isWord(word)){
-			//System.out.println("is a word");
 			allStr.add(word);
 		}
-		//System.out.println("allStr: "+allStr.toString());
+		
+		//obtain all the valid next moves
+		Set<GridPoint> gps = move(r, c, grid.length, grid[0].length);
+		
 		//Check if there is a longer word with the same prefix
-		for(Q1.Moves direction: Q1.Moves.values()){
-			GridPoint gp = move(direction, r, c, grid.length, grid[0].length);
-			if(gp != null && !visited.contains(gp)){
-				ArrayList<GridPoint> visited2 = new ArrayList<GridPoint>();
-				visited2.addAll(visited);
-				Set<String> moreWords = getAllWordsHelper(grid, dictionary, gp.r, gp.c, visited2);
-				if(moreWords != null){
-					allStr.addAll(moreWords);
-				}
+		for(GridPoint gp: gps){			
+			if(!visited.contains(gp)){
+				Set<String> moreWords = getAllWordsHelper(grid, dictionary, gp.r, gp.c, visited, word_temp);
+				visited.remove(visited.size()-1);
+				word_temp.deleteCharAt(word_temp.length()-1);
+				allStr.addAll(moreWords);
 			}
 		}
 		//System.out.println(allStr.toString());
@@ -199,17 +160,17 @@ public class Q1 {
 	}
 
 	public static void main(String[] args) {
-		testNullDictionary();
-		testEmptyDictionary();
-		testNullGrid();
-		testEmptyGrid();
-		testInvalidRowColumnDimension();
-		testOneCellGrid();
-		testNonRepeatationOfCells();
+//		testNullDictionary();
+//		testEmptyDictionary();
+//		testNullGrid();
+//		testEmptyGrid();
+//		testInvalidRowColumnDimension();
+//		testOneCellGrid();
+//		testNonRepeatationOfCells();
 		testAdjacentTraversal();
-		testPrefixWord();//If prefix is a word, traversal should continue to look for a longer word
-		testDepthFirstSearch(); //Test if the depth first search traversal is working properly
-		testExample();
+//		testPrefixWord();//If prefix is a word, traversal should continue to look for a longer word
+//		testDepthFirstSearch(); //Test if the depth first search traversal is working properly
+//		testExample();
 	}
 	
 	@Test
@@ -367,6 +328,7 @@ public class Q1 {
 		result.add("CAR");
 		result.add("CAT");
 		result.add("CARD");
-		assertEquals(result, Q1.getAllWords(n_row, n_column, grid, dictionary));	}
+		assertEquals(result, Q1.getAllWords(n_row, n_column, grid, dictionary));	
+	}
 
 }
